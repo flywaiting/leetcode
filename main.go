31500012541d8983a6dd81f50f550107ecf1cee1
flywaiting <flywaiting@hotmail.com>
2021-04-02 22:30:55 +0800

    for test

--- main.go
+++ main.go
@@ -1,23 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"leetcode/glc"
+)
 
 func main() {
-	clumsy(10)
-}
+	h := []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}
+	ans := glc.Trap(h)
+	fmt.Println(ans)
 
-var remainArr = [...]int{0, 1, 2 * 1, 3 * 2 / 1}
-
-func clumsy(N int) (res int) {
-	idx := N % 4
-	s := 1
-	for N > idx {
-		res += s*N*(N-1)/(N-2) + N - 3
-		s = -1
-		N -= 4
-		fmt.Println(res)
-	}
-	res += s * remainArr[idx]
-	fmt.Println(res)
-	return
 }
