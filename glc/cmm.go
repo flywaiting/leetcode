780fd896b2d70da5b802982d3f0efd148240a459
flywaiting <flywaiting@hotmail.com>
2021-04-08 19:44:00 +0800

    add. 153, binary search

--- glc/cmm.go
+++ glc/cmm.go
@@ -15,3 +15,11 @@ func max(a, b int) int {
 		return b
 	}
 }
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+}
--- main.go
+++ main.go
@@ -7,5 +7,9 @@ import (
 
 func main() {
 
-	fmt.Println(glc.RD80([]int{0, 0, 0, 0, 1, 1, 1, 1, 2, 3, 3}))
+	// fmt.Println(glc.RD80([]int{0, 0, 0, 0, 1, 1, 1, 1, 2, 3, 3}))
+
+	fmt.Println(glc.FM153([]int{3, 4, 5, 1, 2}))
+	fmt.Println(glc.FM153([]int{4, 5, 6, 7, 0, 1, 2}))
+	fmt.Println(glc.FM153([]int{11, 13, 15, 17}))
 }
