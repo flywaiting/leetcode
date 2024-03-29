e394ba6e873af4858cc152f0446401fbc3c43ad7
flywaiting <flywaiting@hotmail.com>
2024-03-29 14:25:51 +0800

    2580, rust fold method

--- rust/src/solution/count_ways_to_group_ovrlapping_ranges_2580.rs
+++ rust/src/solution/count_ways_to_group_ovrlapping_ranges_2580.rs
@@ -1,16 +1,32 @@
+// 将重叠的范围当作一个对象 然后就是个简单的排列组合
 impl Solution {
     pub fn count_ways(ranges: Vec<Vec<i32>>) -> i32 {
         let mut ranges = ranges;
-        ranges.sort_by(|a, b| a[0].cmp(&b[0]));
-        let mut ans: i64 = 1;
-        let mut limit = -1;
-        for p in ranges {
-            if limit < p[0] {
-                ans = ans * 2 % 1_000_000_007;
-            }
-            limit = p[1].max(limit)
-        }
-        ans as i32
+        // ranges.sort_by(|a, b| a[0].cmp(&b[0]));
+        // let mut ans: i64 = 1;
+        // let mut limit = -1;
+        // for p in ranges {
+        //     if limit < p[0] {
+        //         ans = ans * 2 % 1_000_000_007;
+        //     }
+        //     limit = p[1].max(limit)
+        // }
+        // ans as i32
+
+        ranges.sort_unstable_by(|a, b| a[0].cmp(&b[0]));
+        ranges
+            .iter()
+            .fold((1, -1), |(ans, limit), e| {
+                (
+                    if e[0] > limit {
+                        ans * 2 % 1_000_000_007
+                    } else {
+                        ans
+                    },
+                    limit.max(e[1]),
+                )
+            })
+            .0
     }
 }
 
