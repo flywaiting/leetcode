3572adca92309adfcf448ee0b9bce57d595e4c60
flywaiting <flywaiting@hotmail.com>
2024-04-02 22:07:51 +0800

    add 2, 吐了。。。

--- rust/src/solution/add_two_numbers_2.rs
+++ rust/src/solution/add_two_numbers_2.rs
@@ -1,42 +1,31 @@
 use super::config::*;
 
+// 被这个链表恶心坏了
+// option 这个要再琢磨琢磨
+// box 这个倒是挺好理解的
+// 所有权这块 完全被绕晕了。。。
 impl Solution {
     pub fn add_two_numbers(
         l1: Option<Box<ListNode>>,
         l2: Option<Box<ListNode>>,
     ) -> Option<Box<ListNode>> {
-        let head = Box::new(ListNode::new(0));
-        // let tail:Option<Box<ListNode>>;
-        // let tail = head.next;
-        let mut carry = 0;
-        let mut is_end=false;
+        
+        Self::add_numbers(l1,l2, 0)
+    }
 
-        let mut t = (l1, l2);
-        loop {
-            match t {
-                (Some(a), Some(b))=>{
-                    carry += a.val+b.val;
-                    t = (a.next, b.next);
-                },
-                (Some(a), None)|(None, Some(a))=>{
-                    carry += a.val;
-                    t = (a.next, None);
-                },
-                _=> is_end=true;
-            }
-
-            if !is_end || carry!=0 {
-                let node = Box::new(ListNode::new(carry%10));
-                *tail = Some(Box::new(ListNode::new(carry/10)));
-                tail = &mut tail.as_mut();
-
-                carry/=10;
-            }
-            if is_end {
-                break;
-            }
-        };
-        head.next
+    pub fn add_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>, mut other:i32) -> Option<Box<ListNode>> {
+        if l1.is_none() && l2.is_none() && other ==0 {
+            None
+        }else {
+            Some(Box::new(ListNode { 
+                next: Self::add_numbers(
+                    l1.and_then(|a| {other+=a.val; a.next}), 
+                    l2.and_then(|a| {other+=a.val; a.next}),
+                    other/10
+                ),
+                val: other%10  
+            }))
+        }
     }
 }
 
--- todo.md
+++ todo.md
@@ -2,3 +2,8 @@
 ## 需要学习了解的东西
 [ ] iter().enumerate()
 [ ] hashmap 所有权问题
+[ ] option, box, 2
+
+
+## rust 恶心集锦
+- 2 add two numbers, 链表
