40bbcf65f7a47f8ebe616b53cc1f3ef4c07be58f
flywaiting <flywaiting@hotmail.com>
2024-04-08 18:00:51 +0800

    vec to ListNode

--- rust/src/solution/config.rs
+++ rust/src/solution/config.rs
@@ -9,4 +9,16 @@ impl ListNode {
     pub fn new(val: i32) -> Self {
         ListNode { val, next: None }
     }
+
+    pub fn create(list: Vec<i32>) -> Option<Box<ListNode>> {
+        let mut head = Box::new(Self::new(0));
+        // let mut head = Self::new(0);
+        let mut tail = &mut head;
+        // list.reverse();
+        for i in list {
+            tail.next = Some(Box::new(ListNode::new(i)));
+            tail = tail.next.as_mut().unwrap();
+        }
+        head.next
+    }
 }
