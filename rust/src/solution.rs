1f67d8f3a61aee5c257c75d6bbd62b425d56eafd
flywaiting <flywaiting@hotmail.com>
2024-04-16 21:42:15 +0800

    add 19 <linked list>

--- rust/src/solution.rs
+++ rust/src/solution.rs
@@ -15,6 +15,7 @@ pub mod palindrome_linked_list_234;
 pub mod remove_duplicate_node_lcci_0201;
 pub mod remove_duplicates_from_sorted_list_83;
 pub mod remove_linked_list_elements_203;
+pub mod remove_nth_node_from_end_of_list_19;
 pub mod reverse_linked_list_206;
 pub mod two_sum_1;
 pub mod verify_preorder_serialization_of_a_binary_tree_331;
